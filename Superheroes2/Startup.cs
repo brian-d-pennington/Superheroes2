8beeb9262d1f5fe74bef70d86154e2783f237789
Brian Pennington <srsrec@gmail.com>
2018-11-29 09:54:54 -0600

    Restarted Project, Solar Bootstrap Theme, Superhero Model

---
+++ Superheroes2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Superheroes2.Startup))]
+namespace Superheroes2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
